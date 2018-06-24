d855d1b6c44398a017d08fb67bff1762af005ceb
key8297 <key8297@gmail.com>
2018-06-24 22:45:28 +0800

    1045

--- src/api/controllers/user.js
+++ src/api/controllers/user.js
@@ -4,29 +4,6 @@ const User = require('./../models/user');
 const auth = require('../auth/auth');
 
 class UserController{
-    signup(user){
-        let deferred = q.defer();
-        User.findOne({email:user.email})
-        .then((existinguser) => {
-            if(existinguser){
-                deferred.reject("Record already exists.");
-            }
-            else{
-                let data = Object.assign(new User(), user);
-                data.save()
-                .then(user => {
-                    let token = auth.sign({
-                        name: user.name,
-                        email: user.email,
-                        division: user.mainDivision
-                    });
-                    deferred.resolve({token});
-                });
-            }
-        });
-        return deferred.promise;
-    }
-
     login(email, password){
         let deferred = q.defer();
         User.findOne({email, password})
